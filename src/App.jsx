399a8e83eb46b711cc48f5fcd43886c9c59deb53
ShehrozWaseem <shehroz85waseem@gmail.com>
2023-10-04 13:40:22 -0400

    added lab 1 task

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Greeter from './components/Greeter';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Welcome to my first React App</h1>
+      <Greeter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Greeter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Greeter = () => {
+  return (
+    <div>This is a greeter component</div>
+  )
+}
+
+export default Greeter
